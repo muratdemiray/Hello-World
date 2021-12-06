2f8505ea324f06b5f4702408573e4f7b3f14bd61
Demiray <Murat.Demiray@adesso.com.tr>
2021-12-06 08:47:44 +0300

    helm charts updated

--- app/app.py
+++ app/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!!<br> Time is :"+time.strftime('%A %B, %d %Y %H:%M:%S')+"</p>"
+    return "<p>Hello, World!!!<br> Time is :"+time.strftime('%A %B, %d %Y %H:%M:%S')+"</p>"
 
 if __name__== '__main__':    
     #app.run(debug=True)
